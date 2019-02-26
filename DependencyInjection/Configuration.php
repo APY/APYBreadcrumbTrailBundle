247342131df0685c9b96839758ef9c06b2154f25
Mondane <mondane.woodworker@gmail.com>
2019-02-26 21:01:12 +0100

    Add support for symfony < 4.2
    The BC layer is taken from https://github.com/KnpLabs/KnpPaginatorBundle/commit/740b876d61b46de767e457b8b0f0850f3f323dea#diff-850942b3ba24ab03a40aaa81b6152852

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,7 +21,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('apy_breadcrumb_trail');
-        $rootNode = $treeBuilder->getRootNode();
+        // BC layer for symfony/config < 4.2
+        $rootNode = method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('apy_breadcrumb_trail');
 
         $rootNode
             ->children()
