d727c712e8f78046a285b0606c83a8d111663751
kick-the-bucket <kick-the-bucket@users.noreply.github.com>
2020-06-26 16:51:28 +0300

    Add BreadcrumbListener tests

---
+++ tests/EventListener/BreadcrumbListenerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace APY\BreadcrumbTrailBundle\EventListener;
+
+use APY\BreadcrumbTrailBundle\Annotation as APY;
+use APY\BreadcrumbTrailBundle\APYBreadcrumbTrailBundle;
+use APY\BreadcrumbTrailBundle\BreadcrumbTrail\Breadcrumb;
+use Nyholm\BundleTest\BaseBundleTestCase;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class BreadcrumbListenerTest extends BaseBundleTestCase
+{
+    public function controllerProvider()
+    {
+        return [
+            'Traditional' => [[new class {
+                /**
+                 * @APY\Breadcrumb("name")
+                 */
+                public function action()
+                {
+                }
+            }, 'action']],
+            'Invokable' => [new class {
+                /**
+                 * @APY\Breadcrumb("name")
+                 */
+                public function __invoke()
+                {
+                }
+            }],
+        ];
+    }
+
+    protected function getBundleClass()
+    {
+        return APYBreadcrumbTrailBundle::class;
+    }
+
+    protected function setUp(): void
+    {
+        $kernel = $this->createKernel();
+        $kernel->addConfigFile(__DIR__.'/../services.xml');
+        $kernel->addBundle(FrameworkBundle::class);
+        $kernel->boot();
+    }
+
+    /**
+     * @dataProvider controllerProvider
+     *
+     * @param callable $controller
+     */
+    public function testInvokableController($controller)
+    {
+        $container = $this->getContainer();
+        /** @var BreadcrumbListener $listener */
+        $trail = $container->get('APY\BreadcrumbTrailBundle\BreadcrumbTrail\Trail');
+        $listener = $container->get('apy_breadcrumb_trail.annotation.listener');
+        $listener->onKernelController(new ControllerEvent($container->get('kernel'), $controller, new Request(), HttpKernelInterface::MASTER_REQUEST));
+        /** @var Breadcrumb[] $crumbs */
+        $crumbs = iterator_to_array($trail->getIterator());
+        $this->assertCount(1, $crumbs);
+        $this->assertArrayHasKey(0, $crumbs);
+        $this->assertSame('name', $crumbs[0]->title);
+    }
+}
