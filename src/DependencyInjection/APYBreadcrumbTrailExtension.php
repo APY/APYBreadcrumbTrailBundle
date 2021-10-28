7ac4a18995d0aceed03f8c305eeab30849ecb28b
Richard Hagen <richard.hagen@gmail.com>
2021-10-28 17:07:21 +0200

    dont deprecate alias before symfony v. 4.3 Add required symfony/serializer dependency (twig requires it, but doesnt specify it, breaking tests) Move alias deprecation to APYBreadcrumbTrailExtension.php so that we can use the correct symfony version of setDeprecate to avoid deprecation errors

--- src/DependencyInjection/APYBreadcrumbTrailExtension.php
+++ src/DependencyInjection/APYBreadcrumbTrailExtension.php
@@ -11,10 +11,11 @@
 
 namespace APY\BreadcrumbTrailBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\Kernel;
 
 class APYBreadcrumbTrailExtension extends Extension
 {
@@ -30,5 +31,17 @@ class APYBreadcrumbTrailExtension extends Extension
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $this->deprecateService($container, "apy_breadcrumb_trail");
+        $this->deprecateService($container, "apy_breadcrumb_trail.annotation.listener");
+    }
+
+    private function deprecateService(ContainerBuilder $container, string $id) {
+        $alias = $container->getAlias($id);
+        if (Kernel::VERSION_ID >= 50100) {
+            $alias->setDeprecated('APY/BreadcrumbTrailBundle','1.7','The service is deprecated, use "%alias_id%" FQCN as service id instead.');
+        } elseif (Kernel::VERSION_ID >= 40300) {
+            $alias->setDeprecated();
+        }
     }
 }
